1e250eedde43fff984250f91c3213cbe0ac370b8
Oluwaseye.Olofinyo <oluwaseye.olofinyo@andela.co>
2015-01-20 12:11:52 +0100

    changes

--- app/js/custom/route.js
+++ app/js/custom/route.js
@@ -6,25 +6,25 @@ var funspots = angular.module('funspots', ['ngRoute'])
 
     //indexpage route
     .when('/', {
-      templateUrl : 'app/templates/home.html',
+      templateUrl : '/templates/home.html',
       controller : 'homeCtrl'
     })
 
     //homepage route
     .when('/templates/home', {
-      templateUrl : 'app/templates/home.html',
+      templateUrl : '/templates/home.html',
       controller : 'homeCtrl'
     })
 
     //hotspots list page route
     .when('/templates/list', {
-      templateUrl : 'app/templates/list.html',
+      templateUrl : '/templates/list.html',
       controller : 'listCtrl'
     })
 
     //contactPage route
     .when('/templates/form', {
-      templateUrl : 'app/templates/form.html',
+      templateUrl : '/templates/form.html',
       controller : 'formCtrl'
     }).
 
